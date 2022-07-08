3c6114d90683a4136eb9682a3012f7be6444fcbb
anicolellis <alexnicolellis1@gmail.com>
2022-07-08 10:26:16 -0500

    herbie update

--- cosine.py
+++ cosine.py
@@ -4,10 +4,14 @@ import numpy as np
 import pandas as pd
 import math
 import torch
+import random
 
 from pyfuzz.fuzzers import *
 from pyfuzz.byte_mutations import *
 from pyfuzz.fuzz_data_interpreter import *
+
+float_max = 1000000.0
+float_min = -1000000.0
 
 # unstable
 def unstable_cos_similarity(u, v, eps):
@@ -32,19 +36,33 @@ def stable_cos_similarity(u, v, eps):
     result = x / n
     return result
 
+def herbie_cos_similarity(u, v, eps):
+    x = np.sum(np.multiply(u, v))
+    y = np.sum(np.multiply(u, u))
+    z = np.sum(np.multiply(v, v))
+    n = y * z
+    if n < (eps * eps):
+        n = eps * eps
+    n = math.pow(n, -0.5)
+    result = x * n
+    return result
+
+"""
 def unstable_cosine(data):
     fdi = FuzzedDataInterpreter(data)
 
     test1 = np.matrix([
-      [fdi.claim_float(), fdi.claim_float()] ,
-      [fdi.claim_float(), fdi.claim_float()]
+      [float(i) for i in range(84)]
     ])
     test2 = np.matrix([
-      [fdi.claim_float(), fdi.claim_float()] ,
-      [fdi.claim_float(), fdi.claim_float()]
+      [float(i) for i in range(84)]
     ])
 
-    return unstable_cos_similarity(test1, test2, 1e-8)
+    #print(test1, test2)
+
+    result = unstable_cos_similarity(test1, test2, 1e-8)
+    print(result)
+    return result
 
 def stable_cosine(data):
     fdi = FuzzedDataInterpreter(data)
@@ -66,13 +84,56 @@ def stable_cosine(data):
 
 
 if __name__ == "__main__":
-    #runner = FunctionRunner(unstable_cosine)
-    runner = FunctionRunner(stable_cosine)
+    runner = FunctionRunner(unstable_cosine)
+    #runner = FunctionRunner(stable_cosine)
     seed = [bytearray([0] * 12)]
     fuzzer = MutationFuzzer(seed, mutator=mutate_bytes)
-    results = fuzzer.runs(runner, 1000)
+    results = fuzzer.runs(runner, 500)
 
     df = pd.DataFrame(results, columns=["output", "status"])
     print(df.groupby("status").size())
     print("fuzzer.failure_cases:")
     print(fuzzer.failure_cases)
+"""
+
+def create_1D(len, min, max):
+  row = []
+  for _ in range(len):
+    row.append(random.uniform(min, max))
+  return row
+
+def create_2D(len1, len2, min, max):
+  matrix = []
+  for _ in range(len1):
+    matrix.append(create_1D(len2, min, max))
+  return matrix
+
+if __name__ == "__main__":
+  trials = 10
+  length = 5
+  ranges = [(-100.0, 100.0), (float_min, float_max)]
+
+  for min, max in ranges:
+    print('With min as ', min, ' and max as ', max)
+
+    for _ in range(trials):
+      #x = np.matrix(create_1D(length, min, max))
+      x = [13.189142, 8.138781, -4.0982385, 5.143065]
+      #y = np.matrix(create_1D(length, min, max))
+      #y = x
+      y = [13.188879, 8.138888, -4.0983186, 5.1430016]
+
+      print(x, y)
+
+      eps = 1e-12
+
+      print('Unstable: ')
+      print(unstable_cos_similarity(x, y, eps))
+
+      print('Stable: ')
+      print(stable_cos_similarity(x, y, eps))
+
+      print('Herbie: ')
+      print(herbie_cos_similarity(x, y, eps))
+
+      print()
--- softmax.cpp
+++ softmax.cpp
@@ -2,8 +2,14 @@
 #include <math.h> 
 #include <vector>
 #include <algorithm>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
+#define LB 100.0
+#define UB 100000.0
+
+#define DEBUG 1
 
 vector<float> softmax_unstable(const vector<float> &x) 
 {
@@ -14,9 +20,9 @@ vector<float> softmax_unstable(const vector<float> &x)
     for (size_t i = 0; i < x.size(); i++) 
     {
         sum += exp(x[i]);
-        cout << exp(x[i]) <<endl;
+        //cout << exp(x[i]) <<endl;
     }
-    cout << sum << endl;
+    //cout << sum << endl;
     for (size_t j = 0; j < x.size(); j++) 
     {
         result[j] = exp(x[j]) / sum;
@@ -43,9 +49,53 @@ vector<float> softmax_stable(const vector<float> &x)
     return result;
 }
 
+vector<float> softmax_herbie(const vector<float> &x) 
+{
+    float sum = 0;
+    vector<float> result;
+    result.resize(x.size());
+    float max = *max_element(x.begin(), x.end());
+
+    for (size_t i = 0; i < x.size(); i++) 
+    {
+        sum += exp(x[i] - max);
+    }
+    for (size_t j = 0; j < x.size(); j++) 
+    {
+        result[j] = exp(x[j] - log(sum));
+    }
+    return result;
+}
+
+float getFloatInRange()
+{
+    float val = LB + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(UB-LB)));
+    return val;
+}
+
 int main() {
-    vector<float> x{10.0, 100.0, 1000.0};
-    vector<float> y{-1000.0, -10000.0, -1000000.0};
+    srand (static_cast <unsigned> (time(0)));
+    //vector<float> x{10.0, 100.0, 1000.0};
+    //vector<float> y{-1000.0, -10000.0, -1000000.0};
+    vector<float> x(3);
+    //vector<float> y(3);
+
+    generate(begin(x), end(x), getFloatInRange);
+    //generate(begin(y), end(y), getFloatInRange);
+
+    if(DEBUG) 
+    {
+        cout << "Values of x: " << endl;
+        for (const auto &i : x) {
+            cout << i << endl;
+        }
+        /*
+        cout << "Values of y: " << endl;
+        for (const auto &i : y) {
+            cout << i << endl;
+        }
+        */
+    }
 
     vector<float> result_unstable_x = softmax_unstable(x);
     cout << "Unstable softmax given x: " << endl;
@@ -57,7 +107,12 @@ int main() {
     for (const auto &i : result_stable_x) {
         cout << i << endl;
     }
-
+    vector<float> result_herbie_x = softmax_herbie(x);
+    cout << "Herbie softmax given x: " << endl;
+    for (const auto &i : result_herbie_x) {
+        cout << i << endl;
+    }
+    /*
     vector<float> result_unstable_y = softmax_unstable(y);
     cout << "Unstable softmax given y: " << endl;
     for (const auto &i : result_unstable_y) {
@@ -68,4 +123,5 @@ int main() {
     for (const auto &i : result_stable_y) {
         cout << i << endl;
     }
+    */
 }
